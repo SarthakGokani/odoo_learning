1ed739640b77d0281db4ef4b205813d436e760f6
gokanisarthakn@gmail.com <gokanisarthakn@gmail.com>
2020-08-16 19:31:13 +0530

    second try

--- myemployee/models/myemployee.py
+++ myemployee/models/myemployee.py
@@ -9,7 +9,7 @@ class MyEmployee(models.Model):
     first_name = fields.Char(string='First Name')
     last_name = fields.Char(string='Last Name')
     dob = fields.Date(string='Birth Date(DOB)', help='22/08/1998')
-    contact_no = fields.Integer(string='Contact No.', size=10)
+    contact_no = fields.Char(string='Contact No.')
     email = fields.Char(string='Email')
     date_of_joining = fields.Date(string='Date Of Joining', help='04/11/2018')
     wages_type = fields.Selection([('salary', 'Salary'), ('hourlu', 'Hourly')], string = 'Wages Type')
@@ -18,3 +18,5 @@ class MyEmployee(models.Model):
     hobbies = fields.Char(string='Hobbies')
     job_position = fields.Selection([('manager', 'Manager'), ('worker', 'Worker'), ('other', 'Other')], string='Job Position')
     status = fields.Selection([('interview_one', 'Interview One'), ('interview_two', 'Interview Two'), ('hired', 'Hired')], string='Status')
+
+
